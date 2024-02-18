96687b8d70d3dd6da2b5575eef23c2034ce3699c
Vikash Thakur <thakvikash622@gmail.com>
2024-02-18 22:50:01 +0530

    Final commit

--- To Do List/app.js
+++ To Do List/app.js
@@ -13,16 +13,30 @@ function addTask() {
         li.appendChild(span);
     }
     text.value = '';
+    saveTask();
     
 }
 
 list_container.addEventListener("click", function(e){
     if(e.target.tagName === "LI") {
         e.target.classList.toggle("item");
+        saveTask();
     } else if (e.target.tagName === "SPAN") {  
         e.target.parentElement.remove();
+        saveTask();
     }
 }, false);
 
 //In the above code the li and span must be uppercase other wise it's not working
 // So, the reason why the code wasn't working until you made LI and SPAN uppercase is because JavaScript's tagName property returns tag names in uppercase, and you need to match this case when comparing them in your code for the event listener to function as expected.
+
+
+function saveTask () {
+    localStorage.setItem("Data", list_container.innerHTML);
+}
+
+function showTask() {
+    list_container.innerHTML = localStorage.getItem("Data");
+}
+
+showTask();
