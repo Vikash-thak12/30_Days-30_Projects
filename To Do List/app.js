a1610bb0b51b1e39a3a8ee899cd598e9f95163e8
Vikash Thakur <thakvikash622@gmail.com>
2024-02-18 22:43:06 +0530

    Done with the function which will remove or checked the task

--- To Do List/app.js
+++ To Do List/app.js
@@ -15,3 +15,14 @@ function addTask() {
     text.value = '';
     
 }
+
+list_container.addEventListener("click", function(e){
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("item");
+    } else if (e.target.tagName === "SPAN") {  
+        e.target.parentElement.remove();
+    }
+}, false);
+
+//In the above code the li and span must be uppercase other wise it's not working
+// So, the reason why the code wasn't working until you made LI and SPAN uppercase is because JavaScript's tagName property returns tag names in uppercase, and you need to match this case when comparing them in your code for the event listener to function as expected.
