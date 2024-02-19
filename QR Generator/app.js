0b08d4f71382476266b75530afa9a9f12e927ab6
Vikash Thakur <thakvikash622@gmail.com>
2024-02-19 22:17:20 +0530

    Almost done, I have added a new css style shaking of input tag

--- QR Generator/app.js
+++ QR Generator/app.js
@@ -1,10 +1,15 @@
 let qrText = document.querySelector("#qrText");
-let imgbox = document.querySelector("#imgbox");
+let imgbox = document.querySelector(".imgbox");
 let qrImage = document.querySelector("#qrImage");
 api = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
 
 function generateQr() {
     if(qrText.value.length > 0) {
         qrImage.src = api + qrText.value;
+        imgbox.classList.add("show-img");
+        qrText.value= "";
+    } else 
+    {
+        qrText.classList.add("error")
     }
 }
