07875464c3e9072b443a394bdd276332af694872
Vikash Thakur <thakvikash622@gmail.com>
2024-02-20 22:39:46 +0530

    Fixed some small issue in the code

--- Quiz/app.js
+++ Quiz/app.js
@@ -12,16 +12,16 @@ const questions = [
 
 
 const questionElement = document.querySelector(".questions");
-const ansButton = document.querySelectorr(".answer-buttons");
+const ansButton = document.querySelector(".answer-buttons");
 const nextBtn = document.querySelector("#next-btn");
 
 let currentQuestionIndex = 0; 
 let score = 0; 
 
-function startQuiz() {
+function startQuiz(){
     currentQuestionIndex = 0;
     score = 0; 
-    nextBtn.innerHTML = "Click to the next Question:"
+    nextBtn.innerHTML = "Click to the next Question:";
     showQuestion();
 
 }
@@ -32,10 +32,10 @@ function showQuestion() {
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
     
     currentQuestion.answers.forEach( (answer) => {
-        const button = document.createElement(".button");
+        const button = document.createElement("button");
         button.innerHTML = answer.text;
-        button.classList.add(".btn");
-        ansButton.appendchild(button)
+        button.classList.add("btn");
+        ansButton.appendChild(button);
     })
 }
 
