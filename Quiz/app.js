76db0d5d4af5236d75a98518d705aeef7779d17e
Vikash Thakur <thakvikash622@gmail.com>
2024-02-20 22:30:37 +0530

    Created function for startquiz and showquestion

--- Quiz/app.js
+++ Quiz/app.js
@@ -29,5 +29,14 @@ function startQuiz() {
 function showQuestion() {
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.questions
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    
+    currentQuestion.answers.forEach( (answer) => {
+        const button = document.createElement(".button");
+        button.innerHTML = answer.text;
+        button.classList.add(".btn");
+        ansButton.appendchild(button)
+    })
 }
+
+startQuiz();
