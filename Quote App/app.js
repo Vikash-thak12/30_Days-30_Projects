a50d134fafc9e414f5158c0e6ade0f2fc041b184
Vikash Thakur <thakvikash622@gmail.com>
2024-02-25 00:11:29 +0530

    declare two constants

--- Quote App/app.js
+++ Quote App/app.js
@@ -1,4 +1,6 @@
 const api = "https://api.quotable.io/random";
+const quote = document.querySelector("#quote");
+const author = document.querySelector("#author");
 
 async function getQuote(url) {
     const response = await fetch(url);
