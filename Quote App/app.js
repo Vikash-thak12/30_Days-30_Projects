a70d2ad7906a5228c44d89e14808b990653930e0
Vikash Thakur <thakvikash622@gmail.com>
2024-02-25 00:18:22 +0530

    Almost complete the project

--- Quote App/app.js
+++ Quote App/app.js
@@ -1,12 +1,18 @@
 const api = "https://api.quotable.io/random";
 const quote = document.querySelector("#quote");
 const author = document.querySelector("#author");
+const nextBtn = document.querySelector("#next-btn");
 
 async function getQuote(url) {
     const response = await fetch(url);
     let data = await response.json();
     console.log(data);
     quote.innerHTML = data.content;
+    author.innerHTML = data.author;
 }
+
+nextBtn.addEventListener("click", () => {
+    getQuote(api);
+});
 
 getQuote(api);
