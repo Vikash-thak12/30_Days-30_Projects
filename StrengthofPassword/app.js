6f45d92c7843464717fad41c8f4298d57a314479
Vikash Thakur <thakvikash622@gmail.com>
2024-03-01 23:47:28 +0530

    fixing one error of border

---
+++ StrengthofPassword/app.js
@@ -0,0 +1,28 @@
+var password = document.querySelector("#password");
+var message = document.querySelector("#message");
+var str = document.querySelector("#strength");
+var bor = document.querySelector(".input-box")
+
+password.addEventListener("input", () => {
+    if(password.value.length > 0) {
+        message.style.display = "block";
+    } else {
+        message.style.display = "none";
+    }
+
+    if(password.value.length < 4) {
+        str.innerHTML = "Weak";
+        message.style.color = "#ff5925"
+        bor.style.bordercolor = "#ff5925"
+    } else if(password.value.length >= 4 && password.value.length < 8) {
+        str.innerHTML = "medium"
+        message.style.color = "yellow"
+        bor.style.bordercolor = "yellow"
+
+    } else 
+    {
+        str.innerHTML = "Strong"
+        message.style.color = "#26d730";
+        bor.style.borderColor = "#26d730"
+    }
+})
