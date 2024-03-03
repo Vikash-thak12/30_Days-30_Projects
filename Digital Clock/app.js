c2b1203f274c072ee3612292a098845045f8742b
Vikash Thakur <thakvikash622@gmail.com>
2024-03-03 21:10:37 +0530

    Done with the project

--- Digital Clock/app.js
+++ Digital Clock/app.js
@@ -8,7 +8,7 @@ var secs = document.querySelector("#secs");
 setInterval(() => {
     var getTime = new Date();
     // console.log(getTime);
-    hrs.innerHTML = getTime.getHours();
-    mins.innerHTML = getTime.getMinutes();
-    secs.innerHTML = getTime.getSeconds();
+    hrs.innerHTML = (getTime.getHours()<10?"0":"")+getTime.getHours();
+    mins.innerHTML = (getTime.getMinutes()<10?"0":"")+ getTime.getMinutes(); 
+    secs.innerHTML = (getTime.getSeconds()<10?"0":"")+getTime.getSeconds();
 }, 1000);
