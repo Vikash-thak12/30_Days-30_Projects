f32726d26800f09c71c9370c9e297e56bf424234
Vikash Thakur <thakvikash622@gmail.com>
2024-03-03 21:02:37 +0530

    All things are working properly

--- Digital Clock/app.js
+++ Digital Clock/app.js
@@ -2,9 +2,13 @@ var hrs = document.querySelector("#hrs");
 var mins = document.querySelector("#mins");
 var secs = document.querySelector("#secs");
 
-var getTime = new Date();
 
-console.log(getTime);
-hrs.innerHTML = getTime.getHours();
-mins.innerHTML = getTime.getMinutes();
-secs.innerHTML = getTime.getSeconds();
+
+
+setInterval(() => {
+    var getTime = new Date();
+    // console.log(getTime);
+    hrs.innerHTML = getTime.getHours();
+    mins.innerHTML = getTime.getMinutes();
+    secs.innerHTML = getTime.getSeconds();
+}, 1000);
