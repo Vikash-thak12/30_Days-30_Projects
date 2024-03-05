93335516132649a92b14c33232ba8e75e7575a90
Vikash Thakur <thakvikash622@gmail.com>
2024-03-05 14:42:20 +0530

    Almost done with js

--- Launching Rocket/app.js
+++ Launching Rocket/app.js
@@ -1,7 +1,9 @@
-var countDownDate = new Date().getTime;
+var countDownDate = new Date("Oct 24, 2024 00:00:00").getTime();
 var x = setInterval(() => {
-    var currentDate = new Date().getTime;
+    var currentDate = new Date().getTime();
     var distance = countDownDate - currentDate;
+
+    console.log(distance);
 
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -9,9 +11,12 @@ var x = setInterval(() => {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 
-    
+    document.querySelector("#days").innerHTML = days;
+    document.querySelector("#hours").innerHTML = hours;
+    document.querySelector("#minutes").innerHTML = minutes;
+    document.querySelector("#seconds").innerHTML = seconds;
 
 
 
 
-}, 1000);
+}, 1000);   
