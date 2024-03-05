24736b9a97af6024a95f3663fb640fc09241c445
Vikash Thakur <thakvikash622@gmail.com>
2024-03-05 14:20:09 +0530

    calculate time of current and the date of the launching

--- Launching Rocket/app.js
+++ Launching Rocket/app.js
@@ -1,5 +1,17 @@
-var countDownDate = new Date();
-console.log(countDownDate);
-setInterval(() => {
-    console.log(countDownDate)
+var countDownDate = new Date().getTime;
+var x = setInterval(() => {
+    var currentDate = new Date().getTime;
+    var distance = countDownDate - currentDate;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+    
+
+
+
+
 }, 1000);
