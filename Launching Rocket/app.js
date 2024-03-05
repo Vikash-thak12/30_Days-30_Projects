24d6fa11cb01fb3607c74b90c5e309bde5646438
Vikash Thakur <thakvikash622@gmail.com>
2024-03-05 14:57:25 +0530

    Final commit

--- Launching Rocket/app.js
+++ Launching Rocket/app.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Oct 24, 2024 00:00:00").getTime();
+var countDownDate = new Date("Mar 5, 2024 14:57:00").getTime();
 var x = setInterval(() => {
     var currentDate = new Date().getTime();
     var distance = countDownDate - currentDate;
@@ -11,12 +11,27 @@ var x = setInterval(() => {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 
+    days = (days < 10) ? "0" + days : days;
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+
     document.querySelector("#days").innerHTML = days;
     document.querySelector("#hours").innerHTML = hours;
     document.querySelector("#minutes").innerHTML = minutes;
     document.querySelector("#seconds").innerHTML = seconds;
 
 
+    if (distance < 0) {
+        clearInterval(x);
+
+
+        document.querySelector("#days").innerHTML = "00";
+        document.querySelector("#hours").innerHTML = "00";
+        document.querySelector("#minutes").innerHTML = "00";
+        document.querySelector("#seconds").innerHTML = "00";
+    }
 
 
 }, 1000);   
