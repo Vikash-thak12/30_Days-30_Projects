1872a10b82c0a216d1b56656cbc808893135aba3
Vikash Thakur <thakvikash622@gmail.com>
2024-03-07 14:46:31 +0530

    created a async function

--- Image Search Engine/app.js
+++ Image Search Engine/app.js
@@ -1,6 +1,13 @@
-const API = "https://api.unsplash.com/search/photos?page=1&query=office&client_id=K73gs3ZlL7QTic4o8GJe3jpBvJ7xsvQBD5dE-PrU4RA";
+const Accesskey = "K73gs3ZlL7QTic4o8GJe3jpBvJ7xsvQBD5dE-PrU4RA";
 
 const mainform = document.querySelector("#mainform");
 const inputfield = document.querySelector("#inputfield");
 const btn = document.querySelector("#btn");
 const showMoreBtn = document.querySelector("#showMore");
+
+let keyword = "";
+let page = 1;
+async function searchImages(){
+    keyword = inputfield.value;
+    const url = "https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${Accesskey}";
+}
