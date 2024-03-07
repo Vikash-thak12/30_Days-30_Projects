b8c4a4407e4f3eb803dcaa3e6edc714ca525cb40
Vikash Thakur <thakvikash622@gmail.com>
2024-03-07 14:43:15 +0530

    Declaring variables

---
+++ Image Search Engine/app.js
@@ -0,0 +1,6 @@
+const API = "https://api.unsplash.com/search/photos?page=1&query=office&client_id=K73gs3ZlL7QTic4o8GJe3jpBvJ7xsvQBD5dE-PrU4RA";
+
+const mainform = document.querySelector("#mainform");
+const inputfield = document.querySelector("#inputfield");
+const btn = document.querySelector("#btn");
+const showMoreBtn = document.querySelector("#showMore");
