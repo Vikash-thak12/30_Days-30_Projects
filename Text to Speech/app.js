0606212193969834d730a8d4ffc865a9eedb9c34
Vikash Thakur <thakvikash622@gmail.com>
2024-03-08 22:24:27 +0530

    Basic comment of the function or object

--- Text to Speech/app.js
+++ Text to Speech/app.js
@@ -2,9 +2,14 @@ var btn = document.querySelector("#btn");
 var textarea = document.querySelector("#area");
 var select = document.querySelector("#select");
 
+
+// SpeechSynthesisUtterance(); is a part of the Web Speech API in JavaScript that helps the browser speak out text.
 let speech = new SpeechSynthesisUtterance();
 
 btn.addEventListener("click", () => {
     speech.text = textarea.value;
+
+    
+// window.speechSynthesis.speak(speech); is a JavaScript command that instructs the browser to speak the text contained in the      SpeechSynthesisUtterance object
     window.speechSynthesis.speak(speech);
-})
+});
