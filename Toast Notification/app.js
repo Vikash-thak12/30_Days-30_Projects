450b3c76c78d789c8f8c9beb44727df41b0d3fb5
Vikash Thakur <thakvikash622@gmail.com>
2024-03-12 21:27:29 +0530

    Imported three icons

--- Toast Notification/app.js
+++ Toast Notification/app.js
@@ -1,7 +1,7 @@
 let toastbox = document.querySelector("#toastbox");
-var SuccesMsg = 'Successfully Done.';
-var ErroMsg = 'Your message is Error!';
-var InvalidMsg = 'Please enter the valid message...'
+var SuccesMsg = '<i class="fa-solid fa-circle-check"></i> Successfully Done.';
+var ErroMsg = ' <i class="fa-solid fa-circle-xmark"></i> Your message is Error!';
+var InvalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Please enter the valid message...'
 
 function showToast(msg) {
     let toast = document.createElement("div");
