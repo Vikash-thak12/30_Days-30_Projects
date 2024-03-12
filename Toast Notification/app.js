470c1787e4955764b78092d22b480e2b67324c8f
Vikash Thakur <thakvikash622@gmail.com>
2024-03-12 21:24:36 +0530

    Added some properties in showtoast function

--- Toast Notification/app.js
+++ Toast Notification/app.js
@@ -1,2 +1,11 @@
-let toastbox = document.querySelector("#V=toastbox");
+let toastbox = document.querySelector("#toastbox");
+var SuccesMsg = 'Successfully Done.';
+var ErroMsg = 'Your message is Error!';
+var InvalidMsg = 'Please enter the valid message...'
 
+function showToast(msg) {
+    let toast = document.createElement("div");
+    toast.classList.add('toast');
+    toast.innerHTML = msg;
+    toastbox.appendChild(toast);
+}
