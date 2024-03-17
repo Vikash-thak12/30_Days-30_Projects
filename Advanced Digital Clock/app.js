95941f492a6126b57e4d46c37070620d8e5ed961
Vikash Thakur <thakvikash622@gmail.com>
2024-03-17 23:41:45 +0530

    Final commit

--- Advanced Digital Clock/app.js
+++ Advanced Digital Clock/app.js
@@ -13,9 +13,46 @@ function clock() {
     var mins = date.getMinutes();
     var secs = date.getSeconds();
 
-    secdots.innerHTML = '<br>' + ampm + '</br>' + '<h2>' + zero(secs) + '<br><span>Seconds</span></h2>';
-    mindots.innerHTML = '<h2>' + zero(mins) + '<br><span>Minutes</span></h2>';
-    hrdots.innerHTML = '<h2>' + zero(hrs) + '<br><span>Hours</span></h2>';
+
+    var Seconddots = '';
+    for(var i=1; i<61; i++) {
+        var rotation = i * 6;
+        if(i === secs) {
+            Seconddots += '<div class="dot active" style="transform: rotate('+rotation+'deg)"></div>';
+        } else 
+        {
+            Seconddots += '<div class="dot" style="transform: rotate('+rotation+'deg)"></div>';
+        }
+    }
+
+    var minutesdots = '';
+    for(var i=1; i<61; i++) {
+        var rotation = i * 6;
+        if(i === mins) {
+            minutesdots += '<div class="dot active" style="transform: rotate('+rotation+'deg)"></div>';
+        } else 
+        {
+            minutesdots += '<div class="dot" style="transform: rotate('+rotation+'deg)"></div>';
+        }
+    }
+
+
+    
+    var hourdots = '';
+    for(var i=1; i<13; i++) {
+        var rotation = i * 30;
+        if(i === hrs) {
+            hourdots += '<div class="dot active" style="transform: rotate('+rotation+'deg)"></div>';
+        } else 
+        {
+            hourdots += '<div class="dot" style="transform: rotate('+rotation+'deg)"></div>';
+        }
+    }
+    
+
+    secdots.innerHTML = Seconddots + '<b>' + ampm + '</b>' + '<h2>' + zero(secs) + '<br><span>Seconds</span></h2>';
+    mindots.innerHTML = minutesdots + '<h2>' + zero(mins) + '<br><span>Minutes</span></h2>';
+    hrdots.innerHTML = hourdots + '<h2>' + zero(hrs) + '<br><span>Hours</span></h2>';
 }
 
 function zero(number) {
